66db94c126299362aad4885616528c4c5af314b3
trns1997 <trns@ust.hk>
2018-08-08 12:48:42 +0530

    update

--- app/src/main/java/com/example/dank_engine/wifi/MainActivity.java
+++ app/src/main/java/com/example/dank_engine/wifi/MainActivity.java
@@ -20,6 +20,8 @@ import java.util.List;
 
 import static com.example.dank_engine.wifi.R.layout.activity_main;
 import static java.lang.Double.NaN;
+import static java.lang.Double.POSITIVE_INFINITY;
+import static java.lang.Double.doubleToLongBits;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -29,21 +31,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     HashMap<String, Integer> rp2 = new HashMap<>();
     HashMap<String, Integer> rp3 = new HashMap<>();
     HashMap<String, Integer> rp4 = new HashMap<>();
+    HashMap<String, Integer> rp5 = new HashMap<>();
+    HashMap<String, Integer> rp6 = new HashMap<>();
+    HashMap<String, Integer> rp7 = new HashMap<>();
+    HashMap<String, Integer> rp8 = new HashMap<>();
+    HashMap<String, Integer> rp9 = new HashMap<>();
+    HashMap<String, Integer> rp10 = new HashMap<>();
+    HashMap<String, Integer> rp11 = new HashMap<>();
+    HashMap<String, Integer> rp12 = new HashMap<>();
 
-    List<String> rp0_bssids = Arrays.asList("f4:0f:1b:97:fd:30", "f4:0f:1b:93:8b:70", "f4:0f:1b:93:88:c0", "f4:0f:1b:ab:12:c0", "f4:0f:1b:93:a4:30", "f4:0f:1b:97:e1:00", "f4:0f:1b:97:e1:30");
-    List<Integer> rp0_rssi = Arrays.asList(-64, -51, -65, -64, -65, -48, -53);
+    List<String> rp0_bssids = Arrays.asList("18:64:72:22:6b:61", "18:64:72:22:6c:e1", "18:64:72:22:4f:c1", "18:64:72:22:24:c1", "18:64:72:22:b6:81", "18:64:72:22:68:81");
+    List<Integer> rp0_rssi = Arrays.asList(-39, -55, -57, -58, -59, -58);
 
-    List<String> rp1_bssids = Arrays.asList("f4:0f:1b:97:fd:30", "f4:0f:1b:ab:12:c0", "f4:0f:1b:93:a4:30", "f4:0f:1b:97:e1:30", "f4:0f:1b:93:88:c0", "f4:0f:1b:97:e1:00", "f4:0f:1b:97:f9:70");
-    List<Integer> rp1_rssi = Arrays.asList(-59, -59, -58, -47, -58, -50, -63);
+    List<String> rp1_bssids = Arrays.asList("18:64:72:22:6b:61", "18:64:72:22:6c:e1", "18:64:72:22:24:c1", "18:64:72:22:68:81", "18:64:72:22:c1:21", "18:64:72:22:b6:81", "18:64:72:22:4f:c1", "18:64:72:22:6b:81");
+    List<Integer> rp1_rssi = Arrays.asList(-49, -50, -58, -57, -61, -59, -60, -64);
 
-    List<String> rp2_bssids = Arrays.asList("f4:0f:1b:97:e1:30", "f4:0f:1b:93:88:c0", "f4:0f:1b:97:f9:70", "f4:0f:1b:93:a4:30", "f4:0f:1b:97:e1:00", "7c:0e:ce:0e:98:00", "f4:0f:1b:97:f9:e0", "f4:0f:1b:93:8b:70", "f4:0f:1b:ab:12:c0");
-    List<Integer> rp2_rssi = Arrays.asList(-35, -54, -59, -63, -59, -65, -62, -62, -51);
+    List<String> rp2_bssids = Arrays.asList("18:64:72:22:6b:61", "18:64:72:22:6c:e1", "18:64:72:22:24:c1", "18:64:72:22:c1:21", "18:64:72:22:6b:81", "18:64:72:22:c2:41", "18:64:72:22:68:81");
+    List<Integer> rp2_rssi = Arrays.asList(-53, -55, -53, -57, -61, -60, -53);
 
-    List<String> rp3_bssids = Arrays.asList("f4:0f:1b:97:fd:30", "f4:0f:1b:ab:12:c0", "f4:0f:1b:93:88:c0", "f4:0f:1b:97:e1:00", "00:23:eb:3a:14:f0", "f4:0f:1b:93:8b:70", "f4:0f:1b:97:e1:30");
-    List<Integer> rp3_rssi = Arrays.asList(-54, -55, -64, -44, -57, -61, -49);
+    List<String> rp3_bssids = Arrays.asList("18:64:72:22:c1:21", "18:64:72:22:68:81", "18:64:72:22:6b:61", "18:64:72:22:c2:41", "18:64:72:22:24:c1", "18:64:72:22:6c:e1", "18:64:72:22:6b:81", "18:64:72:22:aa:01");
+    List<Integer> rp3_rssi = Arrays.asList(-53, -41, -56, -60, -57, -61, -64, -65);
 
-    List<String> rp4_bssids = Arrays.asList("f4:0f:1b:ab:12:c0", "f4:0f:1b:97:e1:00", "f4:0f:1b:97:f9:70", "f4:0f:1b:93:88:c0", "f4:0f:1b:97:e1:30");
-    List<Integer> rp4_rssi = Arrays.asList(-46, -64, -65, -58, -39);
+    List<String> rp4_bssids = Arrays.asList("00:25:84:86:7b:90", "00:25:84:23:1d:90");
+    List<Integer> rp4_rssi = Arrays.asList(-44, -55);
+
+    List<String> rp5_bssids = Arrays.asList();
+    List<Integer> rp5_rssi = Arrays.asList();
+
+    List<String> rp6_bssids = Arrays.asList();
+    List<Integer> rp6_rssi = Arrays.asList();
+
+    List<String> rp7_bssids = Arrays.asList();
+    List<Integer> rp7_rssi = Arrays.asList();
+
+    List<String> rp8_bssids = Arrays.asList();
+    List<Integer> rp8_rssi = Arrays.asList();
+
+    List<String> rp9_bssids = Arrays.asList();
+    List<Integer> rp9_rssi = Arrays.asList();
+
+    List<String> rp10_bssids = Arrays.asList();
+    List<Integer> rp10_rssi = Arrays.asList();
+
+    List<String> rp11_bssids = Arrays.asList();
+    List<Integer> rp11_rssi = Arrays.asList();
 
 
     @Override
@@ -88,7 +119,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 List<ScanResult> scanRef = wifiManager.getScanResults();
 
                 for (ScanResult result : scanRef) {
-                    if (!result.SSID.equals("eduroam")) {
+                    if (result.level < -65 || !result.SSID.equals("eduroam")) {
                     } else {
                         wifi_info.put(result.BSSID, result.level);
                     }
@@ -103,7 +134,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Double min = euciledian.get(0);
                 int index = 0;
                 for (int i = 0; i < euciledian.size(); i++) {
-                    if (min > euciledian.get(i) && euciledian.get(i) != 0) {
+                    if (min > euciledian.get(i)) {
                         min = euciledian.get(i);
                         index = i;
                     }
@@ -144,6 +175,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public static double comparePoints(HashMap<String, Integer> p1, HashMap<String, Integer> p2) {
         double sum = 0;
         double cnt = 0;
+        double ratio;
         for (String list : p1.keySet()) {
             Integer p2Val = p2.get(list);
             if (p2Val == null) {
@@ -153,9 +185,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             sum += diff * diff;
             cnt++;
         }
-        double ratio = cnt / p1.size();
-//        Log.i("check", ratio + ";       " + cnt + ";        " + p1.size());
-        return (Math.sqrt(sum)) / ratio;
+        if (sum == 0) {
+            sum = 999;
+        }
+        if (cnt == 0) {
+            ratio = 0;
+        } else {
+            ratio = p1.size() / cnt;
+        }
+//        Log.i("check", ratio + ";       " + cnt + ";        " + p1.size() + ";        " + p2.size());
+        return (Math.sqrt(sum) / ratio);
     }
 
     public HashMap<String, Integer> make_rp(List id, List rssi) {
@@ -185,19 +224,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 rp2 = make_rp(rp2_bssids, rp2_rssi);
                 rp3 = make_rp(rp3_bssids, rp3_rssi);
                 rp4 = make_rp(rp4_bssids, rp4_rssi);
+                rp5 = make_rp(rp5_bssids, rp5_rssi);
+                rp6 = make_rp(rp6_bssids, rp6_rssi);
+                rp7 = make_rp(rp7_bssids, rp7_rssi);
+                rp8 = make_rp(rp8_bssids, rp8_rssi);
+                rp9 = make_rp(rp9_bssids, rp9_rssi);
 
                 refPoint.add(rp0);
                 refPoint.add(rp1);
                 refPoint.add(rp2);
                 refPoint.add(rp3);
                 refPoint.add(rp4);
+                refPoint.add(rp5);
+                refPoint.add(rp6);
+                refPoint.add(rp7);
+                refPoint.add(rp8);
+                refPoint.add(rp9);
 
                 TextView location = findViewById(R.id.location);
+                TextView euc = findViewById(R.id.euc);
                 wifiManager.startScan();
                 List<ScanResult> scanRef = wifiManager.getScanResults();
 
                 for (ScanResult result : scanRef) {
-                    if (!result.SSID.equals("eduroam")) {
+                    if (result.level < -65 || !result.SSID.equals("eduroam")) {
                     } else {
                         wifi_info.put(result.BSSID, result.level);
                     }
@@ -209,24 +259,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
 //                Log.i("loc", String.valueOf(euciledian));
 
-                if (euciledian.size() >= (refPoint.size())*10) {
+                if (euciledian.size() >= (refPoint.size()) * 8) {
                     euciledianAvg = rolling(refPoint, euciledian);
 
                     Log.i("loc", String.valueOf(euciledianAvg));
 
-                    Double min = euciledianAvg.get(0);
-                    int index = 0;
+                    Double min = POSITIVE_INFINITY;
+                    int index = 999;
                     for (int i = 0; i < euciledianAvg.size(); i++) {
-                        if (min > euciledianAvg.get(i) && euciledianAvg.get(i) != NaN) {
+                        if (min > euciledianAvg.get(i)) {
                             min = euciledianAvg.get(i);
                             index = i;
                         }
                     }
                     location.setText(String.valueOf(index));
+                    euc.setText(String.valueOf(euciledianAvg));
                     Log.i("loc", String.valueOf(index));
                     euciledian.clear();
                 }
-                handler.postDelayed(this, 100);
+                handler.postDelayed(this, 75);
             }
         };
 
@@ -234,22 +285,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    public List<Double> rolling(List<HashMap> ref, List<Double> euc){
+    public List<Double> rolling(List<HashMap> ref, List<Double> euc) {
         List<Double> avgList = new ArrayList<>();
-        for (int i = 0; i < ref.size(); i++){
+        for (int i = 0; i < ref.size(); i++) {
             double sum = 0;
             double avg = 0;
-            int cnt  = 0;
-            for(int j = 0; j < euc.size(); j++){
-                if(j%ref.size() == i){
+            int cnt = 0;
+            for (int j = 0; j < euc.size(); j++) {
+                if (j % ref.size() == i) {
                     sum += euc.get(j);
                     cnt++;
                 }
             }
 //            Log.i("check", String.valueOf(cnt));
-            avg = sum/cnt;
+            avg = sum / cnt;
             avgList.add(avg);
         }
-        return  avgList;
+        return avgList;
     }
 }
